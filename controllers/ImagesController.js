ce7bae9fc3e0cb44e3a2c3ba077e51b38525e2e3
BlackSponge <blacksponge@tuta.io>
2017-08-20 00:00:18 +0200

    [AJOUT]: Commande !flip_table

---
+++ controllers/ImagesController.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot');
+const TelegramBaseController = Telegram.TelegramBaseController
+const InputFile = Telegram.InputFile
+
+class ImagesController extends TelegramBaseController {
+  flipTableHandler ($) {
+    $.sendDocument(InputFile.byFilePath('imgs/flip_table.gif'))
+  }
+
+  get routes() {
+    return {
+      'flipTableCommand': 'flipTableHandler'
+    }
+  }
+}
+
+module.exports = ImagesController
--- main.js
+++ main.js
@@ -7,13 +7,20 @@ const TextCommand = Telegram.TextCommand
 const fs = require('fs')
 
 const PingController = require('./controllers/PingController')
+const ImagesController = require('./controllers/ImagesController')
 
 const config = JSON.parse(fs.readFileSync('config.json'))
 
 const tg = new Telegram.Telegram(config.apiToken)
 
+
+let imagesController = new ImagesController()
 tg.router
   .when(
     new TextCommand('!ping', 'pingCommand'),
     new PingController()
+  )
+  .when(
+    new TextCommand('!flip_table', 'flipTableCommand'),
+    imagesController
   )
