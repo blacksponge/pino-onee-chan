6ab07bae4f4196d6823be668b84d822a18f03ad0
BlackSponge <blacksponge@tuta.io>
2017-08-20 01:03:38 +0200

    Amélioration de PinoCommandFilter

--- utils/PinoCommandFilter.js
+++ utils/PinoCommandFilter.js
@@ -11,10 +11,23 @@ class PinoCommandFilter extends BaseCommand {
     this._handler = handler
   }
 
+  _checkCommand (inputCommand) {
+    if (inputCommand.indexOf('@') > -1) {
+      let splitedCommand =  inputCommand.split('@')
+
+      return splitedCommand.length === 2 &&
+        splitedCommand[0] === this._commandName &&
+        splitedCommand[1] === 'PinoOneeChanBot'
+
+    } else {
+      return inputCommand === this._commandName
+    }
+  }
+
   test ($) {
     return $.message.text &&
       this._prefix.indexOf($.message.text[0]) > -1 &&
-      $.message.text.slice(1).split(' ')[0] === this._commandName
+      this._checkCommand($.message.text.slice(1).split(' ')[0])
   }
 
   get handlerName () {
