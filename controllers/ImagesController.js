eef412c6caaa0b14a7311bd5019b257337279853
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-08-07 23:40:24 +0900

    Add more advanced search option for /pic

--- controllers/ImagesController.js
+++ controllers/ImagesController.js
@@ -29,20 +29,20 @@ class ImagesController extends TelegramBaseController {
       .filter(el => {return el.length})
       .slice(1)
       .join(' ')
-    let url = 'https://danbooru.donmai.us/posts/random.json'
+    let url = 'https://danbooru.donmai.us/posts.json'
     if (tag) {
-      url += `?tags=${encodeURIComponent(tag)}`
+      url += `?random=true&limit=1&tags=${encodeURIComponent(tag)}`
     }
     getJSON(url, $, (parsedData) => {
-      if (parsedData.file_url) {
-        let options = {caption: `Source https://danbooru.donmai.us/posts/${parsedData.id}`}
+      if (parsedData.length > 0) {
+        let options = {caption: `Source https://danbooru.donmai.us/posts/${parsedData[0].id}`}
 
         let input = InputFile.byUrl(
-          parsedData.file_url,
-          parsedData.file_url
+          parsedData[0].file_url,
+          parsedData[0].file_url
         )
 
-        if (parsedData.file_ext == 'gif') {
+        if (parsedData[0].file_ext == 'gif') {
           $.sendDocument(input, options)
         } else {
           console.log(input)
